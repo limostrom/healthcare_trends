4276586b34dfddb702bd06b838376e87196d746b
Lauren Mostrom <lmostrom@hbs.edu>
2019-09-04 13:26:17 -0400

    Saved R script from Ben Berger as a framework for scraping PubMed

---
+++ query_pubmed.R
@@ -0,0 +1,28 @@
+# Load package for web scraping
+library(rvest)
+
+# Define term
+term = 'cardiovascular disease'
+
+# Clean term for search. e.g. replace whitespace with '%20' for web search
+term_clean = URLencode(term)
+print(term_clean)
+
+# Form URL using the term
+url = paste0('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term=',
+            term_clean, 
+            '&tool=my_tool&email=my_email@example.com'
+            )
+print(url)
+
+
+# Query PubMed and save result
+xml = read_xml(url)
+
+# View structure of output
+print(xml)
+
+# Print Count of Articles
+xml %>% 
+  xml_node('Count') %>% 
+  xml_double()
