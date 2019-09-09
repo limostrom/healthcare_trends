5448e042dceb46db3d7d3ed5ff8984e32a52ca87
Lauren Mostrom <lmostrom@hbs.edu>
2019-09-09 15:08:51 -0400

    Loop over years as well as disease areas

--- execute_queries.R
+++ execute_queries.R
@@ -40,7 +40,7 @@ Q = data.frame(Query = queries, Query_Name = query_names)
 Q$Query = rep(queries_sub$Query, each=length(year_queries))
 Q$Query = paste0(Q$Query,' AND ',year_queries)
 
-pub_counts = sapply(X = Q$Query[1:5], FUN = api_parse)
+pub_counts = sapply(X = Q$Query, FUN = api_parse)
 
 pub_counts = unname(pub_counts)
 
@@ -51,4 +51,8 @@ data = data.frame(
 )
 
 write_csv(data, path = 'Amitabh/PubMed_Search_Results_byDisease.csv')
+
+
+
+
 
